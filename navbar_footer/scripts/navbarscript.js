9956a564e761228362e689ac4792e91631c75191
Andres <romeroseguraandres@gmail.com>
2022-02-16 18:45:51 +0000

    Navbar con margin top

--- navbar_footer/scripts/navbarscript.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    $(".dropdown").hover(function() {
-        var dropdownMenu = $(this).children(".dropdown-menu");
-        if (dropdownMenu.is(":visible")) {
-            dropdownMenu.parent().toggleClass("open");
-        }
-    });
-});
