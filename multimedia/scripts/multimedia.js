50b56d570b115042ef1f163f4d25ff278620c24b
Gabriel Trujillo González <gabrieltrujillogonzalez@alumno.ieselrincon.es>
2022-03-03 10:36:02 +0000

    Adición de contenido a "Revistas"

--- multimedia/scripts/multimedia.js
+++ multimedia/scripts/multimedia.js
@@ -6,7 +6,8 @@ Array.from(albumes).forEach((album) => {
   album.addEventListener('mouseout',function(){
     this.getElementsByTagName('img')[0].style.opacity = '0.8';
   })
-  /*album.addEventListener('click',function(){
-    window.location.href = 'album.html';
-  })*/
 });
+function abrirRevista(revista){
+  const url = 'revistas/'+revista+'.pdf';
+  window.open(url);
+}
